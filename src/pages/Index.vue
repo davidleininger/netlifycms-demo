2937afef20b0dba17105c6fbd5ebb17eb6d4c8d3
David Leininger <davidbleininger@gmail.com>
2019-06-11 23:14:13 -0500

    Update post fee on home. Change Page Title

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,21 @@
 <template>
   <Layout>
+    <main>
+      <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+      <h1>Hello, world!</h1>
 
-    <h1>Hello, world!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      </p>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <div v-for="post in $page.posts.edges" :key="post.path" class="">
-      <g-link :to="post.node.path">{{ post.node.title }}</g-link>
-    </div>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+      <div class="feed">
+        <div v-for="post in $page.posts.edges" :key="post.path">
+          <g-link :to="post.node.path"><h3>{{ post.node.title }}</h3></g-link>
+          <p>{{ post.node.excerpt }}</p>
+        </div>
+      </div>
+    </main>
   </Layout>
 </template>
 
@@ -45,7 +42,7 @@ query Posts ($page: Int) {
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Netlify CMS Starter'
   }
 }
 </script>
