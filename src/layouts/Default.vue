6d2edb444581980b5a9f98100000ae5ff53fcfaa
David Leininger <davidbleininger@gmail.com>
2019-06-11 23:10:50 -0500

    Add Basic Styling

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,11 +5,16 @@
         <g-link to="/">{{ $static.metaData.siteName }}</g-link>
       </strong>
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about">About</g-link>
       </nav>
     </header>
     <slot/>
+    <footer>
+      <p class="footer-links">
+        <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
+        <a href="https://github.com/davidleininger/gridsome-netlifycms-starter" target="_blank" rel="noopener">GitHub</a>
+      </p>
+    </footer>
   </div>
 </template>
 
@@ -22,6 +27,29 @@ query {
 </static-query>
 
 <style>
+:root {
+  --black: #1B2B34;
+  --teal: #5FB3B3;
+  --teal-dark: #59A9A5;
+  --grey: #D8DEE9;
+  --grey-dark: #A4ACB9;
+  --red-light: #ed6f7d;
+  --red: #B3545E;
+  --white: #fff;
+}
+::-webkit-input-placeholder {
+  color: var(--grey-dark);
+}
+*{
+  box-sizing: border-box;
+}
+.flex{
+  display: flex;
+}
+.in-line{
+  display: inline-block;
+}
+
 body {
   font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   margin:0;
@@ -30,10 +58,12 @@ body {
 }
 
 .layout {
-  max-width: 760px;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
   margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
+  max-width: 900px;
+  min-height: 100vh;
+  padding:24px;
 }
 
 .header {
@@ -41,10 +71,43 @@ body {
   justify-content: space-between;
   align-items: center;
   margin-bottom: 20px;
-  height: 80px;
+}
+.logo {
+  color: var(--black);
+  font-size: 1.5rem;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: all 0.2s;
+}
+.logo:hover {
+  color: var(--teal);
 }
 
 .nav__link {
+  color: var(--black);
+  font-size: 1.125rem;
   margin-left: 20px;
+  padding: 0 0.25rem;
+  position: relative;
+  text-decoration: none;
+}
+.nav__link:after {
+  background: var(--teal);
+  bottom: -4px;
+  content: '';
+  height: 2px;
+  left: 0;
+  margin: 0 auto;
+  position: absolute;
+  transform: scaleX(0);
+  transition: transform 0.3s ease;
+  width: 100%;
+}
+.nav__link:hover:after {
+  content: '';
+  transform: scaleX(1);
+}
+.nav__link:hover {
+  color: var(--black);
 }
 </style>
